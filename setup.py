5de01a5d413dc47660c01e3192c98dd11475705d
Mauricio Aizaga <mauricioaizaga@gmail.com>
2015-04-13 10:30:53 -0500

    Update packaging: licence, gitignore, setup

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-# allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-conf-vars',
     version='0.1',
-    packages=['django_admin_conf_vars'],
     include_package_data=True,
     license='BSD License',
     description='A simple Django app to have configuration variables for your project with the Django admin.',
@@ -26,8 +24,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
